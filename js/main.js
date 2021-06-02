ac9d242266abf0e34de6d5c29bed182713fe5652
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2021-06-02 10:59:55 +0300

    add not code comments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно.
+// взято с https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -6,6 +9,11 @@ function getRandomInt(min, max) {
 }
 
 getRandomInt();
+
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+// взято с https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// и
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
 
 function getRandomFloat(min, max, fValue) {
   min;
